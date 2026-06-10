2765749d952076a4078831cb9d4972864229e413
JoaquinHartwig <yanelian29@gmail.com>
2026-06-10 03:01:36 -0300

    Encapsulamiento

---
+++ Aprendizaje/Getters y Setters/Getters y Setters/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design;
+
+namespace Getters_y_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto(502);
+            Console.WriteLine(auto.Velocidad);
+        }
+    }
+    class Auto
+    {
+        private int _velocidad; // Cambiamos la variable de publico a privado y luego utilizamos propiedades
+
+        public Auto(int velocidad)
+        {
+            Velocidad = velocidad;
+        }
+        public int Velocidad
+        {
+            get
+            { return _velocidad; }//Leer
+            set//Asignar un valor con validacion
+            {
+                if (value > 500)
+                {
+                    _velocidad = 500;
+                }
+                else if (value < 0)
+
+                {
+                    _velocidad = 0;
+                }
+                else {
+                    _velocidad = value;
+                        }
+
+
+
+
+                }
+
+
+
+
+            }
+        }
+    }
