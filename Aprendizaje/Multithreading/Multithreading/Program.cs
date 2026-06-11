953d5c67dae2eda86d1220e229ef36f757d7d799
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 00:59:28 -0300

    Multihilos

---
+++ Aprendizaje/Multithreading/Multithreading/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Un hilo es un camino de ejecuccion de un programa
+        {
+            Thread hiloprincipal = Thread.CurrentThread;
+            hiloprincipal.Name = "Hilo principal";
+            Console.WriteLine(hiloprincipal.Name);
+        }
+    }
+}
