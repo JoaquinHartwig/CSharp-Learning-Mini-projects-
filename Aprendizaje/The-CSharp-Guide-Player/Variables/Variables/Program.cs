5b9febfb68ba01c3b1351d8fbcc6865852c71e81
JoaquinHartwig <yanelian29@gmail.com>
2026-06-15 19:44:23 -0300

    Update Program.cs

--- Aprendizaje/The-CSharp-Guide-Player/Variables/Variables/Program.cs
+++ Aprendizaje/The-CSharp-Guide-Player/Variables/Variables/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string username; // Declaring a variable
+            username = Console.ReadLine(); // Assigning a value to a variable
+            Console.WriteLine("Hi " + username); // Retrieving its current value
+            string favoriteColor;
+            favoriteColor = Console.ReadLine();
+            Console.WriteLine("Hi " + favoriteColor);
+            //score = "Generic User"; // DOESN'T COMPILE!  The text "Generic User" is a string, but score’s type is int. 
+            int a;
+            int b;
+            a = 5;
+            b = 2;
+            b = a;
+            a = -3;
+            int x, y, z;
+            int t = 0;  //  you can declare a variable and initialize it on the same line
+            x = y = z = 10; // you can assign the same thing to many variables
+                            //this is not very common
+                            //Console.WriteLine is built to allow it to work with any type
         }
+
     }
 }
