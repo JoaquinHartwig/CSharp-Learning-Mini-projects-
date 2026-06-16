35bbc55b1a70d81edeb717325e48784c37e88dbc
JoaquinHartwig <yanelian29@gmail.com>
2026-06-16 02:11:31 -0300

    The dominion og kings

---
+++ Aprendizaje/The-CSharp-Guide-Player/The Dominion of Kings/The Dominion of Kings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+
+namespace The_Dominion_of_Kings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int estates = Convert.ToInt32(Console.ReadLine());
+            int duchies = Convert.ToInt32(Console.ReadLine());
+            int provinces = Convert.ToInt32(Console.ReadLine());
+
+            int score = estates * 1
+          + duchies * 3
+          + provinces * 6;
+            Console.WriteLine("Your total score is " + score);
+        }
+    }
+}
