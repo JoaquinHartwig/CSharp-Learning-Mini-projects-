d6edc490b343fab05f83c9de399264b39abffe8b
JoaquinHartwig <yanelian29@gmail.com>
2026-06-16 18:20:47 -0300

    Console 2.0

---
+++ Aprendizaje/The-CSharp-Guide-Player/CONSOLE 2.0/CONSOLE 2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CONSOLE_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Game Over");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hola");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            //Console.Clear();//Limpia toda la pantalla
+            Console.Title = "Retro Snake";//permite cambiar el titulo de la ventana
+            Console.Beep(1000, 500);//1000 Hz durante 500 ms
+        }
+    }
+}
