2eb13033b53cbb710ef5f02c2a1233801063f375
JoaquinHartwig <yanelian29@gmail.com>
2026-06-15 21:29:31 -0300

    Yes or yes

---
+++ Aprendizaje/The-CSharp-Guide-Player/The Variable Shop Returns/The Variable Shop Returns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Variable_Shop_Returns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
