c361d9bf533088893674224b925e64c2ea20956f
JoaquinHartwig <yanelian29@gmail.com>
2026-06-17 23:11:23 -0300

    Repairing_the_Clocktower {

--- Aprendizaje/The-CSharp-Guide-Player/DECISION MAKING/DECISION MAKING/Program.cs
+++ Aprendizaje/The-CSharp-Guide-Player/DECISION MAKING/DECISION MAKING/Program.cs
@@ -8,6 +8,16 @@
             if (score == 100) // sin llaves el programa solo interpreta a la primera linea como parte de la condicion if
                 Console.WriteLine("A+!");
                  Console.WriteLine("Perfect score!"); // BUG!
+
+            int x = 10;
+
+            if (x > 5)
+            {
+                int y = 20;
+                Console.WriteLine(y);
+            }
+
+            //Console.WriteLine(y); // ERROR
         }
     }
 }
---
+++ Aprendizaje/The-CSharp-Guide-Player/Repairing the Clocktower/Repairing the Clocktower/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Repairing_the_Clocktower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+
+            }
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+        }
+    }
+}
