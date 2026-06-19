c6059d069817f59e77a275ac14eb4b59bb6bbe5a
JoaquinHartwig <yanelian29@gmail.com>
2026-06-19 16:20:04 -0300

    Update Program.cs

--- Aprendizaje/The-CSharp-Guide-Player/Do While/Do While/Program.cs
+++ Aprendizaje/The-CSharp-Guide-Player/Do While/Do While/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int playersNumber;
+            do
+            {
+                Console.Write("Enter a number between 0 and 10: ");
+                string playerResponse = Console.ReadLine();
+                playersNumber = Convert.ToInt32(playerResponse);
+            }
+            while (playersNumber < 0 || playersNumber > 10);
         }
     }
 }
