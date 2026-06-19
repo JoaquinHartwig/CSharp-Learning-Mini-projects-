42186f032f1e441d3445e5587f5622f2be155e4d
JoaquinHartwig <yanelian29@gmail.com>
2026-06-19 16:16:21 -0300

    While prototipo

---
+++ Aprendizaje/The-CSharp-Guide-Player/THE WHILE LOOP/THE WHILE LOOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace THE_WHILE_LOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playersNumber = -1;//Lo iniciamos en -1 para asegurar que el while se ejecute al menos una vez
+            while (playersNumber < 0 || playersNumber > 10) //Literal termina al poner un numero entre 10 y 0 
+            {
+                Console.Write("Enter a number between 0 and 10: ");
+                string playerResponse = Console.ReadLine();
+                playersNumber = Convert.ToInt32(playerResponse);
+            }
+        }
+    }
+}
