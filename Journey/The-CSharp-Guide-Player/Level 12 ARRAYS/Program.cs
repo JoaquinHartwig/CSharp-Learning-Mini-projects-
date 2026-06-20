b68134ac632bfb09c2db5e1105a1f7b48dab8064
JoaquinHartwig <yanelian29@gmail.com>
2026-06-20 02:23:33 -0300

    commit is a law

--- Journey/The-CSharp-Guide-Player/Level 12 ARRAYS/Program.cs
+++ Journey/The-CSharp-Guide-Player/Level 12 ARRAYS/Program.cs
@@ -6,6 +6,9 @@
         {
             int[] numero = new int[3];
             Console.WriteLine(numero.Length);
+
+            int[] tabla = new int[3];
+            int[] theMiddle = tabla[1..^1];//"Desde el índice 1 hasta antes del último elemento."   
         }
     }
 }
