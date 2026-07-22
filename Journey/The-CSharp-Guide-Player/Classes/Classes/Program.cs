cc09529cd1ac819659b545751ab2cde57c64b362
JoaquinHartwig <yanelian29@gmail.com>
2026-07-22 20:13:52 -0300

    s

---
+++ Journey/The-CSharp-Guide-Player/Classes/Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(2, 3);
+            rectangle.SetWidth(1);
+            Console.WriteLine(rectangle.GetArea());
+        }
+        class Rectangle
+        {
+            private float _width;
+            private float _height;
+            private float _area;
+            public Rectangle(float width, float height)
+            {
+                _width = width;
+                _height = height;
+                _area = _width * _height;
+            }
+            public float GetWidth() => _width;
+            public float GetHeight() => _height;
+            public float GetArea() => _area;
+            public void SetWidth(float value)
+            {
+                _width = value;
+                _area = _width * _height;
+            }
+            public void SetHeight(float value)
+            {
+                _height = value;
+                _area = _width * _height;
+            }
+        }
+    }
+}
