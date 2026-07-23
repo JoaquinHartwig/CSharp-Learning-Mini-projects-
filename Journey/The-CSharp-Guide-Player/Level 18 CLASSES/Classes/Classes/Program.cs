90fc95bc48fb94a2e45005b943196509fdfccada
JoaquinHartwig <yanelian29@gmail.com>
2026-07-23 19:57:19 -0300

    I gonna kill you

--- Journey/The-CSharp-Guide-Player/Level 18 CLASSES/Classes/Classes/Program.cs
+++ Journey/The-CSharp-Guide-Player/Level 18 CLASSES/Classes/Classes/Program.cs
@@ -4,22 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Score best = new Score();
-            best.name = "Test";
-            best.points = 144000;
-            best.level = 15;
-            if(best.EarnedStar())
+            Rectangle rectangle = new Rectangle(2, 3);
+            rectangle.SetWidth(1);
+            Console.WriteLine(rectangle.GetArea());
+        }
+        class Rectangle
+        {
+            private float _width;
+            private float _height;
+            private float _area;
+            public Rectangle(float width, float height)
             {
-                Console.WriteLine("You eaarned a star");
+                _width = width;
+                _height = height;
+                _area = _width * _height;
+            }
+            public float GetWidth() => _width;
+            public float GetHeight() => _height;
+            public float GetArea() => _area;
+            public void SetWidth(float value)
+            {
+                _width = value;
+                _area = _width * _height;
+            }
+            public void SetHeight(float value)
+            {
+                _height = value;
+                _area = _width * _height;
             }
         }
-    }
-    class Score
-    {
-        public string name;
-        public int points;
-        public int level;
-        public bool EarnedStar() => (points / level) > 1000;
-
     }
 }
