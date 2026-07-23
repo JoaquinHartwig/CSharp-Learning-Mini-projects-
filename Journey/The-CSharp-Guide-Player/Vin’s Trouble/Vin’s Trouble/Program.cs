e1d56412148276fdcb34140f537a4f81de2fc762
JoaquinHartwig <yanelian29@gmail.com>
2026-07-22 23:45:31 -0300

    contenido

---
+++ Journey/The-CSharp-Guide-Player/Vin’s Trouble/Vin’s Trouble/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vin_s_Trouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
