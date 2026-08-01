214a2066ee36566ae87304f553f23a591b171ad6
JoaquinHartwig <yanelian29@gmail.com>
2026-08-01 04:52:01 -0300

    kkk

---
+++ Journey/The-CSharp-Guide-Player/The Properties of Arrows/The Properties of Arrows/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Properties_of_Arrows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
